f36fd0eda725893eaf4e351fa0acad38dd26abad
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-27 18:18:48 +0200

    checking the while loop is working

---
+++ mastermind.py
@@ -0,0 +1,8 @@
+def compare_codes():
+    colours = ['black', 'pink', 'red', 'blue', 'yellow', 'green']
+    getGuess = input(f'Enter the code. Pick 4 from the list: {colours}')
+    i = 1
+    while i < len(colours):
+        print(colours[i])
+        i += 1
+compare_codes()
